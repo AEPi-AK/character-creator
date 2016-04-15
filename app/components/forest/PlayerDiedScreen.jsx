f0d77e2e549f47f7b186d3f01ec6382b86ef3495
caroline hermans <carooolinehermans@gmail.com>
2016-04-15 15:11:19 -0400

    player died screen complete

--- app/components/forest/PlayerDiedScreen.jsx
+++ app/components/forest/PlayerDiedScreen.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import SplashScreen from '../SplashScreen.jsx'
 import ScanScreen from '../ScanScreen.jsx'
 
+import './PlayerDiedScreen.less'
+
 class PlayerDiedScreen extends React.Component {
 
     render() {
         return (
             <div className='container'>
-                Player Died
+                <div className='main-title'>You have been defeated</div>
+                <div className='subtitle'>Better luck next time, adventurer!</div>
             </div>
         )
     }
