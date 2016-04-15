00ae058d92b6bc2e1d9c15176c7936830cf7a985
caroline hermans <carooolinehermans@gmail.com>
2016-04-15 13:37:51 -0400

    battle util

---
+++ app/components/forest/battleUtil/battleUtil.js
@@ -0,0 +1,18 @@
+var gaussian = require('./gaussian/lib/gaussian.js')
+
+const PLAYER_HP_MULTIPLIER = 5
+
+function playerHP(level, strength) {
+  return (PLAYER_HP_MULTIPLIER*level*strength)
+}
+
+function playerDamage(level, strength, sDev) {
+  var meanDamage = level*strength
+  var distribution = gaussian(meanDamage, sDev)
+  return Math.floor(distribution.ppf(Math.random()))
+}
+
+function monsterDamage(meanDamage, sDev) {
+  var distribution = gaussian(meanDamage, sDev)
+  return Math.floor(distribution.ppf(Math.random()))
+}
